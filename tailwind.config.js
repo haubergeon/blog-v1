f7357bdaf47c0a5b57d26f1fa6630cd2b71c0a8a
haubergeon <gsingh6_be19@thapar.edu>
2021-05-14 22:06:22 +0530

    added navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 module.exports = {
   purge: [],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'media', // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'aqua-blue' : '#00C2CB',
+        'dark-turq' : '#03989E',
+        'turq-blue' : '#5CE1E6',
+        'aqa' : '#7FFFD4'
+      },
+      fontFamily: {
+        'nav': 'Syne'
+      }
+    },
   },
   variants: {
     extend: {},
