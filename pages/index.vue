686ab10db4b35204c981a5e8151a0852ef2a1ce9
haubergeon <gsingh6_be19@thapar.edu>
2021-05-15 19:38:16 +0530

    added images

--- pages/index.vue
+++ pages/index.vue
@@ -8,7 +8,7 @@
     <div class="card">
     <img class="object-cover" src="../assets/Open Source.png" alt="">
     <div class="m-4">
-      <span class="text-lg bg-aqua-blue">A Noob's guide to Dual Booting.</span>
+      <span class="text-lg text-black">A Noob's guide to Dual Booting.</span>
     </div>
     <div class="badge">
       <span class="flex items-center">
@@ -21,9 +21,9 @@
 
 
     <div class="card">
-    <img class="object-cover" src="../assets/Open Source.png" alt="">
+    <img class="object-cover" src="../assets/lockdown.png" alt="">
     <div class="m-4">
-      <span class="text-lg bg-aqua-blue">A Noob's guide to Dual Booting.</span>
+      <span class="text-lg text-black">The ultimate source of validation.</span>
     </div>
     <div class="badge">
       <span class="flex items-center">
@@ -35,9 +35,9 @@
 
 
     <div class="card">
-    <img class="object-cover" src="../assets/Open Source.png" alt="">
+    <img class="object-cover" src="../assets/electronics.png" alt="">
     <div class="m-4">
-      <span class="text-lg bg-aqua-blue">A Noob's guide to Dual Booting.</span>
+      <span class="text-lg text-black">What's it going to be? 0 or 1.</span>
     </div>
     <div class="badge">
       <span class="flex items-center">
@@ -48,9 +48,9 @@
     </div>
 
     <div class="card">
-    <img class="object-cover" src="../assets/Open Source.png" alt="">
+    <img class="object-cover" src="../assets/run.png" alt="">
     <div class="m-4">
-      <span class="text-lg bg-aqua-blue">A Noob's guide to Dual Booting.</span>
+      <span class="text-lg text-black">Running your first 21kms.</span>
     </div>
     <div class="badge">
       <span class="flex items-center">
@@ -62,67 +62,7 @@
     
   </div>
 
-  <h1 class ="bg-blue-400 rounded-md p-2">Popular Articles</h1>
-
-  <div class="m-8 grid lg:grid-cols-4 gap-10">
-
-
-    <div class="card">
-    <img class="object-cover" src="../assets/Open Source.png" alt="">
-    <div class="m-4">
-      <span class="text-lg bg-aqua-blue">A Noob's guide to Dual Booting.</span>
-    </div>
-    <div class="badge">
-      <span class="flex items-center">
-        <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clip-rule="evenodd"></path></svg>
-      </span>
-      <span class="flex items-center">10 mins</span>
-    </div>
-    </div>
-
-
-
-    <div class="card">
-    <img class="object-cover" src="../assets/Open Source.png" alt="">
-    <div class="m-4">
-      <span class="text-lg bg-aqua-blue">A Noob's guide to Dual Booting.</span>
-    </div>
-    <div class="badge">
-      <span class="flex items-center">
-        <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clip-rule="evenodd"></path></svg>
-      </span>
-      <span class="flex items-center">10 mins</span>
-    </div>
-    </div>
-
-
-    <div class="card">
-    <img class="object-cover" src="../assets/Open Source.png" alt="">
-    <div class="m-4">
-      <span class="text-lg bg-aqua-blue">A Noob's guide to Dual Booting.</span>
-    </div>
-    <div class="badge">
-      <span class="flex items-center">
-        <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clip-rule="evenodd"></path></svg>
-      </span>
-      <span class="flex items-center">10 mins</span>
-    </div>
-    </div>
-
-    <div class="card">
-    <img class="object-cover" src="../assets/Open Source.png" alt="">
-    <div class="m-4">
-      <span class="text-lg bg-aqua-blue">A Noob's guide to Dual Booting.</span>
-    </div>
-    <div class="badge">
-      <span class="flex items-center">
-        <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clip-rule="evenodd"></path></svg>
-      </span>
-      <span class="flex items-center">10 mins</span>
-    </div>
-    </div>
-    
-  </div>
+  
 
 
 
