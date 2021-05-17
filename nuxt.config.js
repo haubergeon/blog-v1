10754aa2e46faa131b19d7b35f356c59a2e7139c
haubergeon <gsingh6_be19@thapar.edu>
2021-05-17 22:38:10 +0530

    changed title

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Binary Bones',
+    title: 'Binary Bias',
     htmlAttrs: {
       lang: 'en'
     },
@@ -19,7 +19,6 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
-
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
   ],
